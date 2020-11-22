96bc02a4393c9aea3e4b5fa4e2fefb285fa08a6b
usuario <alu0101437538@ull.edu.es>
2020-11-22 20:03:04 +0000

    Todos los ejercicios hechos

--- vector_statistic/vector_func.cc
+++ vector_statistic/vector_func.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cmath>
 
-double media_vector (std::vector<double> vector, int lenght) {
+double media_vector (std::vector<double> vector, int lenght) {      //Calcular la media
   double media;
   for(int i = 0; i < lenght; ++i) {
     media = media + vector[i];
@@ -10,11 +10,11 @@ double media_vector (std::vector<double> vector, int lenght) {
 return media / lenght;
 } 
 
-double desv_standard (double media) {
+double desv_standard (double media) {                               //Calcular la desviación típica
 return sqrt(media);
 } 
 
-double max_value (std::vector<double> vector, int lenght) {
+double max_value (std::vector<double> vector, int lenght) {         //Calcular el valor máximo
   double maxvalue;
   for(int i = 0; i < lenght; ++i) {
     if (i == 0) {
@@ -27,7 +27,8 @@ double max_value (std::vector<double> vector, int lenght) {
 return maxvalue;
 }
 
-double min_value (std::vector<double> vector, int lenght) {
+double min_value (std::vector<double> vector, int lenght) {         //Calcular el valor mínimo
+  double maxvalue;
   double minvalue;
   for(int i = 0; i < lenght; ++i) {
     if (i == 0) {
